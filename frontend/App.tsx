be240d0a302fd1c6ad65d8042a8fef2d5c2509c5
Soporte <soporte@example.com>
2026-05-16 02:02:05 -0500

    fix: polyfill atob and btoa for react native

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,7 +3,14 @@ import { AppNavigator } from './src/presentacion/navigation/AppNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Text, TextInput } from 'react-native';
+import { decode, encode } from 'base-64';
 
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 
 export default function App() {
